f2cf4906fece47ee90c86ab45c4951107c726c01
Jacob Handwerger <jstudyeth@gmail.com>
2023-03-08 11:41:26 -0500

    leisures populating works

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 const User = require('../models/User');
 
-router.get('/dashboard/:userId', (req, res, next) => {
+router.get('/dashboard', (req, res, next) => {
   User.findById(req.params.userId)
   .populate('tasks')
   .populate('leisures')
       .then((foundUser) => {
-        res.json(foundUser)
+        res.json( "This is the found user JSON:", foundUser)
       })
       .catch((err) => {
         console.log(err)
