90ea99a442aec9c1982f59a99cb8f92a1262c860
Jacob Handwerger <jstudyeth@gmail.com>
2023-03-10 05:56:59 -0500

    bug fixing

--- routes/users.js
+++ routes/users.js
@@ -47,20 +47,24 @@ router.put("/subtract/points/:id", async (req, res, next) => {
     const user = await User.findById(req.params.id);
     const leisure = await Leisure.findById(req.body.leisureId);
 
-    if (leisure.added === false) {
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.id,
-        { points: user.points - req.body.points },
-        { new: true }
-      );
-      await Leisure.findByIdAndUpdate(
-        req.body.leisureId,
-        { added: true },
-        { new: true }
-      );
-      return res.status(200).json(updatedUser);
-    }else{
-      return res.status(400).json({msg: "You've already added this leisure"})
+    if (leisure.cost < user.points) {
+      if (leisure.added === false) {
+        const updatedUser = await User.findByIdAndUpdate(
+          req.params.id,
+          { points: user.points - req.body.points },
+          { new: true }
+        );
+        await Leisure.findByIdAndUpdate(
+          req.body.leisureId,
+          { added: true },
+          { new: true }
+        );
+        return res.status(200).json(updatedUser);
+      } else{
+        return res.status(400).json({msg: "You've already added this leisure"})
+      }
+    } else {
+      return res.status(400).json({msg: "Not enough points!"})
     }
   } catch (error) {
     console.log(error);
